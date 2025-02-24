9d4309731ce0cf9555178aed82cad004e8c46f47
inceselim <inceselim91@gmail.com>
2025-02-24 16:36:24 +0300

    Revert "++"
    This reverts commit 0fe47f79622ea951fa9601199aed709b5b26669d.

--- android/app/src/main/java/com/countriesincesoy/MainActivity.kt
+++ android/app/src/main/java/com/countriesincesoy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.countrieswarincesoy
+package com.countriesincesoy
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/countriesincesoy/MainApplication.kt
+++ android/app/src/main/java/com/countriesincesoy/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.countrieswarincesoy
+package com.countriesincesoy
 
 import android.app.Application
 import com.facebook.react.PackageList
