d2db4f2261a853dd7a48c548ef4b1040d33a777c
inceselim <inceselim91@gmail.com>
2025-02-27 12:52:42 +0300

    ++

--- android/app/src/main/java/com/countriesincesoy/MainActivity.kt
+++ android/app/src/main/java/com/countriesincesoy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.countriesincesoy
+package com.countriesincesoyselim
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/countriesincesoy/MainApplication.kt
+++ android/app/src/main/java/com/countriesincesoy/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.countriesincesoy
+package com.countriesincesoyselim
 
 import android.app.Application
 import com.facebook.react.PackageList
